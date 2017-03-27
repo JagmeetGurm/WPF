58c17193760465692d528f7ab80f819770d7e718
JagmeetGurm <jagmeetgurm@yahoo.com>
2017-03-26 18:55:49 -0700

    adds INotifyPropertyChange and IDataErrorInfo

---
+++ WpfRevisit/SimpleErrorMessageDisplay.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfRevisit
+{
+  public  class SimpleErrorMessageDisplay: INotifyPropertyChanged, IDataErrorInfo
+    {
+      private string _name;
+      public string Name
+      {
+          get { return _name; }
+          set { _name = value; OnPropertyChanged("Name"); }
+      }
+
+      public string Error
+      {
+          get { return null; }
+      }
+
+      public string this[string columnName]
+      {
+          get
+          {
+              string errorMessage = string.Empty;
+              if (columnName == "Name")
+              {
+                  if (this.Name == string.Empty)
+                  {
+                      errorMessage = "Please enter a name";
+                  }
+              }
+              return errorMessage;
+          }
+      }
+      #region PropertyChange
+      public event PropertyChangedEventHandler PropertyChanged;
+      private void OnPropertyChanged(String propertyName)
+      {
+          if (PropertyChanged != null)
+          {
+              PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+          }
+      }
+      #endregion PropertyChange
+    }
+}
